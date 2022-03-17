3f4e5573660838af765ccc8f65cf82b67fa23af8
houdini91 <mdstrauss91@gmail.com>
2022-03-17 12:18:58 +0200

    installer action

---
+++ installer/scripts/Linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$(id --user)" -eq "0" ]; then
+    sudocmd=""
+else
+    sudocmd="sudo"
+fi
+$sudocmd apt --quiet update --assume-yes || true
+$sudocmd apt --quiet install --assume-yes curl apt-transport-https
+
+curl --silent --show-error https://scribesecuriy.jfrog.io/artifactory/api/security/keypair/scribe-artifactory/public | $sudocmd apt-key add -
+echo -e "machine https://scribesecuriy.jfrog.io/\nlogin $ARTIFACTORY_USERNAME\npassword $ARTIFACTORY_PASSWORD" | $sudocmd tee /etc/apt/auth.conf.d/scribe > /dev/null
+echo -e "Package: bomber\nPin: release n=stable\nPin-Priority: 900" | $sudocmd tee /etc/apt/preferences.d/scribe_bomber > /dev/null
+echo 'deb https://scribesecuriy.jfrog.io/artifactory/scribe-debian-local stable non-free'
+echo 'deb https://scribesecuriy.jfrog.io/artifactory/scribe-debian-local stable non-free' | $sudocmd tee /etc/apt/sources.list.d/scribe.list > /dev/null
+$sudocmd apt --quiet update --assume-yes || true
+$sudocmd apt --quiet install --assume-yes bomber
