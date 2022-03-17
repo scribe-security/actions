28ac8802b6819275fd48c4e18c887179f0e0b29c
houdini91 <mdstrauss91@gmail.com>
2022-03-17 12:36:20 +0200

    test

--- installer/scripts/Linux.sh
+++ installer/scripts/Linux.sh
@@ -9,7 +9,11 @@ $sudocmd apt --quiet update --assume-yes || true
 $sudocmd apt --quiet install --assume-yes curl apt-transport-https
 
 curl --silent --show-error https://scribesecuriy.jfrog.io/artifactory/api/security/keypair/scribe-artifactory/public | $sudocmd apt-key add -
+
+if [[ ! -z "${ARTIFACTORY_USERNAME}" ]]  && [ ! -z "${ARTIFACTORY_PASSWORD}" ] ; then
+echo "Adding username password"
 echo -e "machine https://scribesecuriy.jfrog.io/\nlogin $ARTIFACTORY_USERNAME\npassword $ARTIFACTORY_PASSWORD" | $sudocmd tee /etc/apt/auth.conf.d/scribe > /dev/null
+fi 
 echo -e "Package: bomber\nPin: release n=stable\nPin-Priority: 900" | $sudocmd tee /etc/apt/preferences.d/scribe_bomber > /dev/null
 echo 'deb https://scribesecuriy.jfrog.io/artifactory/scribe-debian-local stable non-free'
 echo 'deb https://scribesecuriy.jfrog.io/artifactory/scribe-debian-local stable non-free' | $sudocmd tee /etc/apt/sources.list.d/scribe.list > /dev/null
