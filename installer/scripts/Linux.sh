b5870ee2f972cdbd862d02ba31ee4d50df1bf787
houdini91 <mdstrauss91@gmail.com>
2022-06-30 11:44:35 +0300

    test

--- installer/scripts/Linux.sh
+++ installer/scripts/Linux.sh
@@ -18,7 +18,7 @@ echo "Adding username password"
 echo -e "machine https://scribesecuriy.jfrog.io/\nlogin $ARTIFACTORY_USERNAME\npassword $ARTIFACTORY_PASSWORD" | $sudocmd tee /etc/apt/auth.conf.d/scribe > /dev/null
 fi 
 
-echo -e "Package: bomber\nPin: release n=stable\nPin-Priority: 900" | $sudocmd tee /etc/apt/preferences.d/scribe_bomber > /dev/null
+echo -e "Package: $TOOL_NAME\nPin: release n=stable\nPin-Priority: 900" | $sudocmd tee /etc/apt/preferences.d/scribe > /dev/null
 # Truth is i am not sure why our debian are mapped to x86_64 on artifactory and not the amd64 arch ... 2DO fix this somehow..
 echo 'deb [arch=x86_64] https://scribesecuriy.jfrog.io/artifactory/scribe-debian-local stable non-free'
 echo 'deb [arch=x86_64] https://scribesecuriy.jfrog.io/artifactory/scribe-debian-local stable non-free' | $sudocmd tee /etc/apt/sources.list.d/scribe.list > /dev/null
@@ -28,12 +28,12 @@ $sudocmd apt update -o Dir::Etc::sourcelist=/etc/apt/sources.list.d/scribe.list 
 apt show bomber || true | grep -q scribe 
 if [ $? -eq 0 ] ; then
     if [[ ! -z "${VERSION}" ]] ; then
-    $sudocmd apt --quiet install --assume-yes bomber=$VERSION
+    $sudocmd apt --quiet install --assume-yes $TOOL_NAME=$VERSION
     else
-    $sudocmd apt --quiet install --assume-yes bomber
+    $sudocmd apt --quiet install --assume-yes $TOOL_NAME
     fi
 else
-    echo "Scribe bomber could not be found"
+    echo "Scribe $TOOL_NAME could not be found"
     exit 1
 fi
 
