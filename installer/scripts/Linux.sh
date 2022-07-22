09b55ab50878bebfeb869f81b789488b5ecfe5ba
houdini91 <mdstrauss91@gmail.com>
2022-07-22 12:47:29 +0300

    test

--- installer/scripts/Linux.sh
+++ installer/scripts/Linux.sh
@@ -18,10 +18,13 @@ echo "Adding username password"
 echo -e "machine https://scribesecuriy.jfrog.io/\nlogin $ARTIFACTORY_USERNAME\npassword $ARTIFACTORY_PASSWORD" | $sudocmd tee /etc/apt/auth.conf.d/scribe > /dev/null
 fi 
 
-# echo -e "Package: $TOOL_NAME\nPin: release n=stable\nPin-Priority: 900" | $sudocmd tee /etc/apt/preferences.d/scribe > /dev/null
-# Truth is i am not sure why our debian are mapped to x86_64 on artifactory and not the amd64 arch ... 2DO fix this somehow..
 echo 'deb https://scribesecuriy.jfrog.io/artifactory/scribe-debian-local stable non-free'
-echo 'deb https://scribesecuriy.jfrog.io/artifactory/scribe-debian-local stable non-free' | $sudocmd tee /etc/apt/sources.list.d/scribe.list > /dev/null
+echo 'deb  https://scribesecuriy.jfrog.io/artifactory/scribe-debian-local stable non-free' | $sudocmd tee /etc/apt/sources.list.d/scribe.list > /dev/null
+
+# 2DO - remove arch after porting to amd64 distribution complete
+echo 'deb  [arch=x86_64] https://scribesecuriy.jfrog.io/artifactory/scribe-debian-local stable non-free'
+echo 'deb  [arch=x86_64]  https://scribesecuriy.jfrog.io/artifactory/scribe-debian-local stable non-free' | $sudocmd tee /etc/apt/sources.list.d/scribe.list > /dev/null
+
 
 $sudocmd apt update -o Dir::Etc::sourcelist=/etc/apt/sources.list.d/scribe.list || true
 
